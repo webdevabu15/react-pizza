d28df33b55ca9c172f538356debce77b1f247cff
Abubakr <abubakrgulomov859@gmail.com>
2024-07-14 17:45:55 +0500

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, '../public/db.json')); // db.json faylini to'g'ri yo'l bilan ko'rsatish
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
