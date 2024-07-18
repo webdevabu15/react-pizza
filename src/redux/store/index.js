36269a4181b9cb73cde5235012cb2b867f0dbcc1
Abubakr <abubakrgulomov859@gmail.com>
2024-07-18 11:06:45 +0500

    a

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,18 +1,20 @@
-import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
-import {cart} from '../reducer/cart';
-import dataReducer from '../reducer/dataReducer';
-import filters from '../reducer/filter';
+import { createStore, compose, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import { cart } from "../reducer/cart";
+import dataReducer from "../reducer/dataReducer";
+import filters from "../reducer/filter";
 
 const rootReducer = combineReducers({
-    data: dataReducer,
-    cart,
-    filters,
+  data: dataReducer,
+  cart,
+  filters,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export { store };
