7626a5e542f885a4cc907d3f6ea60d2f5d1edf98
Abubakr <abubakrgulomov859@gmail.com>
2024-07-23 10:50:38 +0500

    persist added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.scss";
-import { store } from "./redux/store";
-
+// import { store } from "./redux/store";
+import { store, persistor } from "./redux/store/index";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
         <App />
       </BrowserRouter>
+        </PersistGate>
   </Provider>
 );
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, compose, combineReducers, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import {thunk} from "redux-thunk";
 import { cart } from "../reducer/cart";
 import dataReducer from "../reducer/dataReducer";
 import filters from "../reducer/filter";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const rootReducer = combineReducers({
   data: dataReducer,
@@ -10,11 +12,21 @@ const rootReducer = combineReducers({
   filters,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: ["data", "cart"], // saqlamoqchi bo'lgan reducerlarni ko'rsatishingiz mumkin
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
